78227aaa086a0355473f5f0237b5b1e9b1238753
ZImeni <zimeni22@gmail.com>
2018-05-17 20:50:19 +0800

    first commit

---
+++ app/src/main/java/com/limore/zhibek/to_dum/TaksActivity.java
@@ -0,0 +1,13 @@
+package com.limore.zhibek.to_dum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taks);
+    }
+}
